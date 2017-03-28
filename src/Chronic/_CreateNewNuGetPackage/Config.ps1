3283ff5a9f3e4c1e98f26c53ff44e2d1d862bd73
hgb-mtemkine <mtemkine@hellogbye.com>
2017-03-28 17:10:27 -0400

    MT - update version

--- src/Chronic/_CreateNewNuGetPackage/Config.ps1
+++ src/Chronic/_CreateNewNuGetPackage/Config.ps1
@@ -16,11 +16,11 @@
 # Specify the Version Number to use for the NuGet package. If not specified, the version number of the assembly being packed will be used.
 # NuGet version number guidance: https://docs.nuget.org/docs/reference/versioning and the Semantic Versioning spec: http://semver.org/
 # e.g. "" (use assembly's version), "1.2.3" (stable version), "1.2.3-alpha" (prerelease version).
-$versionNumber = "0.3.13"
+$versionNumber = "0.3.15"
 
 # Specify any Release Notes for this package. 
 # These will only be included in the package if you have a .nuspec file for the project in the same directory as the project file.
-$releaseNotes = "Fixed bug in handling of '2nd week in March'"
+$releaseNotes = "Fixed bug in handling of 'in 7 days from' (ignore from)"
 
 # Specify a specific Configuration and/or Platform to only create a NuGet package when building the project with this Configuration and/or Platform.
 #	e.g. $configuration = "Release"
